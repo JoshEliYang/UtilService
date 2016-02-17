1f982d1300e26bf3e6849b4c1e2f48d8640d32c9
joshy <joshy@gobeta.com.cn>
2016-02-17 12:59:25 +0800

    delete unused code

--- src/main/java/cn/springmvc/dao/PriceTagDAO.java
+++ src/main/java/cn/springmvc/dao/PriceTagDAO.java
@@ -5,18 +5,6 @@ import java.util.List;
 import cn.springmvc.model.PriceTag;
 
 public interface PriceTagDAO {
-
-	public int insertPriceTag(PriceTag pt);
-	
-	public List<PriceTag> selectAllPriceTags();
-	
-	public int deletePriceTag(String id);
-	
-	public int updatePriceTag(PriceTag pt);
-	
-	public List<PriceTag> selectPriceTagsByParams(PriceTag pt);
-	
-	public PriceTag selectPriceTagsById(String id);
 	
 	public List<PriceTag> selectPriceTagsByShopId(String shopId);
 }
--- src/main/java/cn/springmvc/service/PriceTagService.java
+++ src/main/java/cn/springmvc/service/PriceTagService.java
@@ -6,18 +6,6 @@ import cn.springmvc.model.PriceTag;
 
 public interface PriceTagService {
 	
-	public int insertPriceTag(PriceTag pt) throws Exception;
-	
-	public List<PriceTag> selectAllPriceTags() throws Exception;
-	
-	public int deletePriceTag(String id) throws Exception;
-	
-	public int updatePriceTag(PriceTag pt) throws Exception;
-	
-	public List<PriceTag> selectPriceTagsByParams(PriceTag pt) throws Exception;
-	
-	public PriceTag selectPriceTagById(String id) throws Exception;
-	
 	public List<PriceTag> selectPriceTagsByShopId(String shopId) throws Exception;
 
 }
