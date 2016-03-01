bcb4524443f2f42e475696b76c552564e0c528c5
joshy <joshy@gobeta.com.cn>
2016-03-01 15:41:25 +0800

    re 3-1 move these models in project

---
+++ src/main/java/cn/springmvc/model/DailySalesAnalysis.java
@@ -0,0 +1,127 @@
+package cn.springmvc.model;
+
+public class DailySalesAnalysis {
+
+	private String ORDER_CREATE_DATE;
+
+	private String dayWeek;
+
+	private String CLIENT_TYPENAME;
+
+	private String STORE_TYPENAME;
+
+	private String STORE_NAME;
+
+	private String ORDER_PRICE;
+
+	private String ORDER_NUMBER;
+
+	private String ORDERAMOUNT;
+
+	private String ALLYH;
+
+	private String PAYMENT_AMOUNT;
+
+	public String getORDER_CREATE_DATE() {
+		return ORDER_CREATE_DATE;
+	}
+
+	public void setORDER_CREATE_DATE(String oRDER_CREATE_DATE) {
+		ORDER_CREATE_DATE = oRDER_CREATE_DATE;
+	}
+
+	public String getDayWeek() {
+		return dayWeek;
+	}
+
+	public void setDayWeek(String dayWeek) {
+		this.dayWeek = dayWeek;
+	}
+
+	public String getCLIENT_TYPENAME() {
+		return CLIENT_TYPENAME;
+	}
+
+	public void setCLIENT_TYPENAME(String cLIENT_TYPENAME) {
+		CLIENT_TYPENAME = cLIENT_TYPENAME;
+	}
+
+	public String getSTORE_TYPENAME() {
+		return STORE_TYPENAME;
+	}
+
+	public void setSTORE_TYPENAME(String sTORE_TYPENAME) {
+		STORE_TYPENAME = sTORE_TYPENAME;
+	}
+
+	public String getSTORE_NAME() {
+		return STORE_NAME;
+	}
+
+	public void setSTORE_NAME(String sTORE_NAME) {
+		STORE_NAME = sTORE_NAME;
+	}
+
+	public String getORDER_PRICE() {
+		return ORDER_PRICE;
+	}
+
+	public void setORDER_PRICE(String oRDER_PRICE) {
+		ORDER_PRICE = oRDER_PRICE;
+	}
+
+	public String getORDER_NUMBER() {
+		return ORDER_NUMBER;
+	}
+
+	public void setORDER_NUMBER(String oRDER_NUMBER) {
+		ORDER_NUMBER = oRDER_NUMBER;
+	}
+
+	public String getORDERAMOUNT() {
+		return ORDERAMOUNT;
+	}
+
+	public void setORDERAMOUNT(String oRDERAMOUNT) {
+		ORDERAMOUNT = oRDERAMOUNT;
+	}
+
+	public String getALLYH() {
+		return ALLYH;
+	}
+
+	public void setALLYH(String aLLYH) {
+		ALLYH = aLLYH;
+	}
+
+	public String getPAYMENT_AMOUNT() {
+		return PAYMENT_AMOUNT;
+	}
+
+	public void setPAYMENT_AMOUNT(String pAYMENT_AMOUNT) {
+		PAYMENT_AMOUNT = pAYMENT_AMOUNT;
+	}
+
+	public DailySalesAnalysis(String oRDER_CREATE_DATE, String dayWeek,
+			String cLIENT_TYPENAME, String sTORE_TYPENAME, String sTORE_NAME,
+			String oRDER_PRICE, String oRDER_NUMBER, String oRDERAMOUNT,
+			String aLLYH, String pAYMENT_AMOUNT) {
+		super();
+		ORDER_CREATE_DATE = oRDER_CREATE_DATE;
+		this.dayWeek = dayWeek;
+		CLIENT_TYPENAME = cLIENT_TYPENAME;
+		STORE_TYPENAME = sTORE_TYPENAME;
+		STORE_NAME = sTORE_NAME;
+		ORDER_PRICE = oRDER_PRICE;
+		ORDER_NUMBER = oRDER_NUMBER;
+		ORDERAMOUNT = oRDERAMOUNT;
+		ALLYH = aLLYH;
+		PAYMENT_AMOUNT = pAYMENT_AMOUNT;
+	}
+
+	public DailySalesAnalysis() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/cn/springmvc/model/SaleTopAnalysis.java
@@ -0,0 +1,173 @@
+package cn.springmvc.model;
+
+// resources ： r_base_dict_item、e_order_info、e_order_extinfo、e_order_detail
+public class SaleTopAnalysis {
+
+	private String ORDER_CREATE_DATE;
+
+	private String STORE_TYPENAME;
+
+	private String STORE_NAME;
+
+	private String CLIENT_TYPENAME;
+
+	private String goods_class_name;
+
+	private String CARGO_NAME;
+
+	private String purchaseAmount;
+
+	private String PAYAMOUNT;
+
+	private String saleCount;
+
+	private String SALEAMOUNT;
+
+	private String totalRate;
+
+	private String ratePercent;
+
+	private String jinbuCount;
+
+	private String jinbuAmount;
+
+	public String getORDER_CREATE_DATE() {
+		return ORDER_CREATE_DATE;
+	}
+
+	public void setORDER_CREATE_DATE(String oRDER_CREATE_DATE) {
+		ORDER_CREATE_DATE = oRDER_CREATE_DATE;
+	}
+
+	public String getSTORE_TYPENAME() {
+		return STORE_TYPENAME;
+	}
+
+	public void setSTORE_TYPENAME(String sTORE_TYPENAME) {
+		STORE_TYPENAME = sTORE_TYPENAME;
+	}
+
+	public String getSTORE_NAME() {
+		return STORE_NAME;
+	}
+
+	public void setSTORE_NAME(String sTORE_NAME) {
+		STORE_NAME = sTORE_NAME;
+	}
+
+	public String getCLIENT_TYPENAME() {
+		return CLIENT_TYPENAME;
+	}
+
+	public void setCLIENT_TYPENAME(String cLIENT_TYPENAME) {
+		CLIENT_TYPENAME = cLIENT_TYPENAME;
+	}
+
+	public String getGoods_class_name() {
+		return goods_class_name;
+	}
+
+	public void setGoods_class_name(String goods_class_name) {
+		this.goods_class_name = goods_class_name;
+	}
+
+	public String getCARGO_NAME() {
+		return CARGO_NAME;
+	}
+
+	public void setCARGO_NAME(String cARGO_NAME) {
+		CARGO_NAME = cARGO_NAME;
+	}
+
+	public String getPurchaseAmount() {
+		return purchaseAmount;
+	}
+
+	public void setPurchaseAmount(String purchaseAmount) {
+		this.purchaseAmount = purchaseAmount;
+	}
+
+	public String getPAYAMOUNT() {
+		return PAYAMOUNT;
+	}
+
+	public void setPAYAMOUNT(String pAYAMOUNT) {
+		PAYAMOUNT = pAYAMOUNT;
+	}
+
+	public String getSaleCount() {
+		return saleCount;
+	}
+
+	public void setSaleCount(String saleCount) {
+		this.saleCount = saleCount;
+	}
+
+	public String getSALEAMOUNT() {
+		return SALEAMOUNT;
+	}
+
+	public void setSALEAMOUNT(String sALEAMOUNT) {
+		SALEAMOUNT = sALEAMOUNT;
+	}
+
+	public String getTotalRate() {
+		return totalRate;
+	}
+
+	public void setTotalRate(String totalRate) {
+		this.totalRate = totalRate;
+	}
+
+	public String getRatePercent() {
+		return ratePercent;
+	}
+
+	public void setRatePercent(String ratePercent) {
+		this.ratePercent = ratePercent;
+	}
+
+	public String getJinbuCount() {
+		return jinbuCount;
+	}
+
+	public void setJinbuCount(String jinbuCount) {
+		this.jinbuCount = jinbuCount;
+	}
+
+	public String getJinbuAmount() {
+		return jinbuAmount;
+	}
+
+	public void setJinbuAmount(String jinbuAmount) {
+		this.jinbuAmount = jinbuAmount;
+	}
+
+	public SaleTopAnalysis(String oRDER_CREATE_DATE, String sTORE_TYPENAME,
+			String sTORE_NAME, String cLIENT_TYPENAME, String goods_class_name,
+			String cARGO_NAME, String purchaseAmount, String pAYAMOUNT,
+			String saleCount, String sALEAMOUNT, String totalRate,
+			String ratePercent, String jinbuCount, String jinbuAmount) {
+		super();
+		ORDER_CREATE_DATE = oRDER_CREATE_DATE;
+		STORE_TYPENAME = sTORE_TYPENAME;
+		STORE_NAME = sTORE_NAME;
+		CLIENT_TYPENAME = cLIENT_TYPENAME;
+		this.goods_class_name = goods_class_name;
+		CARGO_NAME = cARGO_NAME;
+		this.purchaseAmount = purchaseAmount;
+		PAYAMOUNT = pAYAMOUNT;
+		this.saleCount = saleCount;
+		SALEAMOUNT = sALEAMOUNT;
+		this.totalRate = totalRate;
+		this.ratePercent = ratePercent;
+		this.jinbuCount = jinbuCount;
+		this.jinbuAmount = jinbuAmount;
+	}
+
+	public SaleTopAnalysis() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
